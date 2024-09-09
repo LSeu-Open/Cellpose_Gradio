a6db4fb06eee5750ff94e40b1294f2081b26b219
Lucas <95351758+LSeu-Open@users.noreply.github.com>
2024-09-09 14:11:03 +0200

    Update run_app.bat to venv

--- run_app.bat
+++ run_app.bat
@@ -1,18 +1,14 @@
 @echo off
+setlocal enabledelayedexpansion
 
-REM Set the path to the Anaconda installation directory
-set ANACONDA_PATH=C:\Users\your_username\AppData\Local\anaconda3
+REM Set the path to the virtual environment
+set VENV_PATH=.\cellpose-gradio-env
 
-REM Set the path to the Anaconda Scripts directory
-set SCRIPTS_PATH=%ANACONDA_PATH%\Scripts
-
-REM Name of the Conda environment
-set ENV_NAME=cellpose-gradio
-
-REM Activate the environment
-call conda activate %ENV_NAME%
+REM Activate the virtual environment
+call %VENV_PATH%\Scripts\activate
 if %errorlevel% neq 0 (
-    echo Failed to activate Conda environment.
+    echo Failed to activate virtual environment.
+    pause
     exit /b 1
 )
 
