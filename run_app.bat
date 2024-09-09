17e88c2a094afbbcb3540cfeb732ab7293dd931f
Lucas <95351758+LSeu-Open@users.noreply.github.com>
2024-09-09 13:41:10 +0200

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,22 +1,24 @@
 @echo off
-setlocal enabledelayedexpansion
 
-:: Name of the Conda environment
+REM Set the path to the Anaconda installation directory
+set ANACONDA_PATH=C:\Users\your_username\AppData\Local\anaconda3
+
+REM Set the path to the Anaconda Scripts directory
+set SCRIPTS_PATH=%ANACONDA_PATH%\Scripts
+
+REM Name of the Conda environment
 set ENV_NAME=cellpose-gradio
 
-:: Activate the environment
+REM Activate the environment
 call conda activate %ENV_NAME%
 if %errorlevel% neq 0 (
     echo Failed to activate Conda environment.
     exit /b 1
 )
 
-:: Launch the Gradio app
+REM Launch the Gradio app
 echo Launching Cellpose Gradio app...
 python Cellpose_gradio.py
 
-:: Deactivate the environment
-call conda deactivate
-
-echo App closed.
+REM Pause to keep the window open
 pause
